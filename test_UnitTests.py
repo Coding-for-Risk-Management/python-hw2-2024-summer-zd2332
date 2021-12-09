dd064c31905f9bb55d5e926ee2c1b703c7433d98
David Romoff <DaveTheMovie@gmail.com>
2021-12-09 17:51:50 -0500

    Create HW 2 by removing from HW1
    Vectorized version of 3 questions from HW1

--- test_UnitTests.py
+++ test_UnitTests.py
@@ -11,22 +11,6 @@ import BondDuration_File
 def test_getBondDuration():
     assert round(BondDuration_File.getBondDuration(.03, 2000000, .04, 10,1),2) == 8.51
 
-import BondPrice_E_File
-def test_GetBondPriceE():
-    yc = [.010,.015,.020,.025,.030]
-    face = 2000000
-    couponRate = .04
-    assert round(BondPrice_E_File.getBondPrice_E(face, couponRate, yc)) == 2098949 
-
-import BondPrice_Z_File
-def test_GetBondPriceZ():
-    yc = [.010,.015,.020,.025,.030]
-    times=[1,1.5,3,4,7]
-    face = 2000000
-    couponRate = .04
-    x = BondPrice_Z_File.getBondPrice_Z(face, couponRate, times, yc)
-    assert round(x) == 1996533
-    
 import FizzBuzz_File
 def test_FizzBuzz():
     x = FizzBuzz_File.FizzBuzz(40,45)
